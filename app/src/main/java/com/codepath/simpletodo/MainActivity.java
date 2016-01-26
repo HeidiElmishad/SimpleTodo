640844475500b75dcd0630b8b7790f05b9c183c3
Heidi Elmishad <pair+heidi@projectchelsea.com>
2016-01-25 21:48:22 -0800

    updates code to use a custom adapter

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -2,8 +2,11 @@ package com.codepath.simpletodo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +17,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.codepath.simpletodo.com.codepath.simpletodo.ItemsAdapter;
+import com.codepath.simpletodo.com.codepath.simpletodo.TodoItem;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.apache.commons.io.FileUtils;
@@ -25,8 +30,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 10;
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
+//    private ArrayList<String> items;
+//    private ArrayAdapter<String> itemsAdapter;
+    private ItemsAdapter mItemsAdapter = null;
     private ListView lvItems;
     private EditText editText;
     private Button btnAddItem;
@@ -47,21 +53,57 @@ public class MainActivity extends AppCompatActivity {
 
         readItems();
         lvItems = (ListView) findViewById(R.id.lvItems);
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        lvItems.setAdapter(itemsAdapter);
 
-        btnAddItem = (Button) findViewById(R.id.btnAddItem);
-        btnAddItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText = (EditText) findViewById(R.id.etNewItem);
-                String itemText = editText.getText().toString();
-                itemsAdapter.add(itemText);
-                editText.setText("");
-                writeItems();
-            }
-        });
+        // Construct the data source
+        ArrayList<TodoItem> mItemsArray = new ArrayList<TodoItem>();
+        // Create the adapter to convert the array to views
+        mItemsAdapter = new ItemsAdapter(this,R.layout.todo_item ,mItemsArray);
+        // Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.lvItems);
+        listView.setAdapter(mItemsAdapter);
+
+
+
+//        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+//        lvItems.setAdapter(itemsAdapter);
+
+//        btnAddItem = (Button) findViewById(R.id.btnAddItem);
+//        btnAddItem.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                editText = (EditText) findViewById(R.id.etNewItem);
+//                String itemText = editText.getText().toString();
+//                TodoItem mTodoItem = new TodoItem(null, null, null, itemText,null);
+//                mItemsAdapter.add(mTodoItem);
+//                editText.setText("");
+//                writeItems();
+//            }
+//        });
         setupListViewListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add) {
+            addNewTask();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addNewTask() {
+        intent = new Intent(this, AddTaskActivity.class);
+        startActivityForResult(intent,REQUEST_CODE);
+
     }
 
     private void setupListViewListener() {
@@ -69,20 +111,20 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                items.remove(position);
-                writeItems();
-                itemsAdapter.notifyDataSetChanged();
-                Toast.makeText(getBaseContext(),"Item removed!", Toast.LENGTH_LONG).show();
+//                mItemsAdapter.remove(position);
+//                writeItems();
+//                mItemsAdapter.notifyDataSetChanged();
+//                Toast.makeText(getBaseContext(),"Item removed!", Toast.LENGTH_LONG).show();
                 return true;
             }
         });
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                POSITION = position;
-                String itemText = items.get(position).toString();
-                intent.putExtra("itemText", itemText);
-                startActivityForResult(intent, REQUEST_CODE);
+//                POSITION = position;
+//                String itemText = items.get(position).toString();
+//                intent.putExtra("itemText", itemText);
+//                startActivityForResult(intent, REQUEST_CODE);
             }
         });
     }
@@ -90,60 +132,59 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            if (data.hasExtra("editedText")) {
-                String result = data.getExtras().getString("editedText");
-                if (result != null && result.length() > 0) {
-                    items.remove(POSITION);
-                    items.add(POSITION,result);
-                    writeItems();
-                    itemsAdapter.notifyDataSetChanged();
-                }
+            String itemName = null;
+            String itemNotes= null;
+            String itemPriority = null;
+            String itemStatus = null;
+            if(data.hasExtra("taskName")) {
+                itemName = data.getExtras().getString("taskName");
             }
+            if(data.hasExtra("taskNotes")) {
+                itemNotes = data.getExtras().getString("taskNotes");
+            }
+            if(data.hasExtra("taskPriority")) {
+                itemPriority = data.getExtras().getString("taskPriority");
+            }
+            if(data.hasExtra("taskstatus")) {
+                itemStatus = data.getExtras().getString("taskstatus");
+            }
+
+            TodoItem item = new TodoItem(null,itemPriority,itemStatus,itemName,itemNotes);
+            mItemsAdapter.add(item);
+//            mItemsAdapter.notifyDataSetChanged();
+            writeItems();
+//            if (data.hasExtra("editedText")) {
+//                String result = data.getExtras().getString("editedText");
+//                if (result != null && result.length() > 0) {
+//                    items.remove(POSITION);
+//                    items.add(POSITION,result);
+//                    writeItems();
+//                    itemsAdapter.notifyDataSetChanged();
+//                }
         }
     }
 
     private void readItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try{
-            items = new ArrayList<String>(FileUtils.readLines(todoFile));
-        }
-        catch (IOException e){
-            items = new ArrayList<String>();
-        }
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//        try{
+//            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+//        }
+//        catch (IOException e){
+//            items = new ArrayList<String>();
+//        }
     }
 
     private void writeItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            FileUtils.writeLines(todoFile, items);
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//        try {
+//            FileUtils.writeLines(todoFile, items);
+//        }
+//        catch (IOException e){
+//            e.printStackTrace();
+//        }
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
